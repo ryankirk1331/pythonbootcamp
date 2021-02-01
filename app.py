f71ff6b57a833a6d12edcf20f1375a87896a988b
Ryan Kirkpatrick <ryan@pop-os.localdomain>
2021-02-01 06:57:18 -0600

    while loops 2

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
-num = int(input())
-i = num
-cnt = 1
-if num == 0:
-    cnt = 1 
-
-elif num < 0:
-    i *= -1
-
-while i > 0:
-    i //= 10
-    cnt +=1
-    i -= 1
-
-print(f"# of digits in {num} = {cnt}")
-
-         
+n = int(input())
+num = 0
+while num <= n:
+    if num % 3 == 0 and num % 4 == 0 or num % 8 == 0:
+        print(num, end=' ')
+    num +=1
