2efd81dcb518feab21ba3f3cf484498a60587dac
Ryan Kirkpatrick <ryan@pop-os.localdomain>
2021-02-06 08:59:40 -0600

    pre pull

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-# # Practice / Pair of Numbers / My Answer *********
 
-N, M, sum = map(int, input().split())
-res = 0
-
-for a in range(1, N+1):
-    for b in range(1, M, 1):
-        if a + b == sum:
-            res +=1
-print(res)
